514b50980af0d8987b22008a9135dd14f844ba39
dangdang122 <vmfhxhtm122@naver.com>
2025-03-17 11:54:14 +0900

    변경

--- src/chapter02/Code01.java
+++ src/chapter02/Code01.java
@@ -5,7 +5,7 @@ public class Code01 {
         System.out.println("오늘 주석배우고");
         System.out.print("줄바꿈 기능 차이점과\n연결연산자 배웠습니다\n");
         System.out.println("얏호!"+271+127);
-        System.out.print(1002+154+"=1156");
+        System.out.print(1002+154 +"=1156");
     }
 
 }
