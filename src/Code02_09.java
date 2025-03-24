4666094de242a65c8cc1c464ff14eb528840ff24
dangdang122 <vmfhxhtm122@naver.com>
2025-03-24 11:33:59 +0900

    변경

---
+++ src/Code02_09.java
@@ -0,0 +1,19 @@
+
+
+
+public class Code02_09 {
+
+
+
+    public static void main(String[] args){
+        int num1 = 10, num2 = 20;
+        double result;
+        result = num1 + num2;
+        System.out.printf("%d + %d = %.1f\n", num1, num2, result);
+
+        //정수형 끼리 계산하면 정수만 저장
+        //정수형 둘 중 하나를 실수형 변환 후 연산
+        result = num1 / (double)num2;
+        System.out.printf("%d / %d = %.1f\n", num1, num2, result);
+    }
+}
