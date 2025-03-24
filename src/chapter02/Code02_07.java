9dddcb5e9a968490bc083502539ec3aa13b25b45
dangdang122 <vmfhxhtm122@naver.com>
2025-03-24 10:44:04 +0900

    변경

---
+++ src/chapter02/Code02_07.java
@@ -0,0 +1,21 @@
+package chapter02;
+
+public class Code02_07 {
+    public static void main(String[] args){
+        int a = 2;
+        int b = 10;
+
+        int qwer = a + b;
+        int asdf = a - b;
+        int zxcv = a * b;
+        int tyui = b / a;
+
+        System.out.printf("%d + %d = %d\n", a, b, qwer);
+        System.out.printf("%d - %d = %d\n", a, b, asdf);
+        System.out.printf("%d * %d = %d\n", a, b, zxcv);
+        System.out.printf("%d / %d = %d\n", a, b, tyui);
+
+
+
+    }
+}
